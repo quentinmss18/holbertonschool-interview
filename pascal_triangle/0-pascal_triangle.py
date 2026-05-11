5097789723c54776fe8b89d75e051705a2652f46
quentinmss <quentinmoussous@gmail.com>
2026-05-11 11:26:12 +0200

    Refactor pascal_triangle function for clarity

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""
+Module 0-pascal_triangle
+"""
+
+
 def pascal_triangle(n):
     """
     Retourne une liste de listes d'entiers représentant le triangle de Pascal de n.
@@ -5,25 +11,17 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    # Initialisation avec la première ligne
     triangle = [[1]]
 
-    # On boucle de 1 à n-1 pour créer les lignes suivantes
     for i in range(1, n):
-        prev_row = triangle[-1]  # La ligne précédente
-        # La nouvelle ligne commence toujours par 1
+        prev_row = triangle[-1]
         current_row = [1]
         
-        # On calcule les éléments entre les deux extrémités
         for j in range(1, i):
-            # Somme des deux éléments au-dessus
             val = prev_row[j - 1] + prev_row[j]
             current_row.append(val)
         
-        # La nouvelle ligne se termine toujours par 1
         current_row.append(1)
-        
-        # On ajoute la ligne terminée au triangle
         triangle.append(current_row)
 
     return triangle
